1716d534c20e6228529c23f94bea115b710b4380
João Pedro <joaopedroaat@outlook.com>
2024-11-14 22:58:50 -0300

    Initial commit

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,15 @@
+return {
+  "stevearc/oil.nvim",
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  opts = {
+    default_file_explorer = true,
+  },
+  lazy = false,
+  -- Optional dependencies
+  dependencies = { { "echasnovski/mini.icons", opts = {} } },
+  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+  keys = {
+    { "-", "<cmd>Oil<CR>", desc = "Oil" },
+  },
+}
