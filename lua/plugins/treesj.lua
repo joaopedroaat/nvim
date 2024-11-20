c8418d65713e1d4fa84251b924c09c34a8a67547
João Pedro <joaopedroaat@outlook.com>
2024-11-19 23:56:28 -0300

    Add treesj

---
+++ lua/plugins/treesj.lua
@@ -0,0 +1,15 @@
+return {
+  "Wansmer/treesj",
+  keys = {
+    {
+      "<leader>m",
+      "<cmd>TSJToggle<cr>",
+      desc = "Toggle Treesitter Join",
+    },
+  },
+  cmd = { "TSJToggle", "TSJSplit", "TSJJoin" },
+  opts = {
+    use_default_keymaps = false,
+  },
+  dependencies = { "nvim-treesitter/nvim-treesitter" }, -- if you install parsers with `nvim-treesitter`
+}
