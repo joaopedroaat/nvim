225089c3218a2226888a539f6843cb48ff0882c5
João Pedro <joaopedroaat@outlook.com>
2024-12-27 00:00:45 -0300

    Add htmx support

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -2,5 +2,11 @@ return {
   "neovim/nvim-lspconfig",
   opts = {
     inlay_hints = { enabled = false },
+    servers = {
+      html = {},
+      cssls = {},
+      emmet_ls = {},
+      htmx = {},
+    },
   },
 }
